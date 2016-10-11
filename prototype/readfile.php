d0706f4234529ccf2d821bd24f036e4e9bb53445
Kalilo <kalilohansmann@gmail.com>
2016-10-11 13:57:20 +0200

    addig more case handles for if and only if

--- prototype/readfile.php
+++ prototype/readfile.php
@@ -119,6 +119,36 @@
 				$fn_rules[] = "\telse";
 				$fn_rules[] = "\t\tset_var({$r[1]}, 1);";
 			}
+			else if ($q == "&") {
+				$r[1] = trim($r[1]);
+				$tmp = explode("+", $r[1]);
+				$tmp[0] = trim($tmp[0]);
+				$tmp[1] = trim($tmp[1]);
+				$fn_rules[] = "\tif ({$r[0]})";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 1, 1);";
+				$fn_rules[] = "\telse";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 0, 0);";
+			}
+			else if ($q == "|") {
+				$r[1] = trim($r[1]);
+				$tmp = explode("|", $r[1]);
+				$tmp[0] = trim($tmp[0]);
+				$tmp[1] = trim($tmp[1]);
+				$fn_rules[] = "\tif ({$r[0]})";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 1, 1);";
+				$fn_rules[] = "\telse";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 0, 0);";
+			}
+			else if ($q == "^") {
+				$r[1] = trim($r[1]);
+				$tmp = explode("^", $r[1]);
+				$tmp[0] = trim($tmp[0]);
+				$tmp[1] = trim($tmp[1]);
+				$fn_rules[] = "\tif ({$r[0]})";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 1, 1);";
+				$fn_rules[] = "\telse";
+				$fn_rules[] = "\t\tset_and_case({$tmp[0]}, {$tmp[1]}, 0, 0);";
+			}
 			//
 		}
 		else {
